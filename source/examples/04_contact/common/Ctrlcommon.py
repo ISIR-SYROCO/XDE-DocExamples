83362f9f931f68e31c6f5625a0d9b48760e945b6
joseph salini windows <josephsalini@gmail.com>
2012-04-25 23:57:36 +0200

    add a new robot and simplify common file

---
+++ source/examples/04_contact/common/Ctrlcommon.py
@@ -0,0 +1,66 @@
+
+import rtt_interface
+import dsimi.rtt
+import physicshelper
+
+import lgsm
+
+import time
+
+#-------------------------------------------------------------------------------
+#
+# Create a new Task. This will be the Kinematic controller.
+#
+#-------------------------------------------------------------------------------
+class ContactController(dsimi.rtt.Task):
+  
+    def __init__(self, taskName, world, robotName):
+        task = rtt_interface.PyTaskFactory.CreateTask(taskName)
+        dsimi.rtt.Task.__init__(self, task)
+
+        # model instance
+        self.model = physicshelper.createDynamicModel(world, robotName)
+
+        # create input ports
+        self.c_in = self.addCreateInputPort("cont", "SMsg", True)
+        self.c_ok = False
+        self.c = None
+        
+        self.tau_out = self.addCreateOutputPort("tau", "VectorXd")
+
+
+    def startHook(self):
+        pass
+  
+    def stopHook(self):
+        pass
+  
+    def updateHook(self):
+        if not self.c_ok:
+            self.c,self.c_ok = self.c_in.read()
+        
+        if self.c_ok:
+            self.c_ok = False
+            self.doUpdate(self.c)
+
+  
+    def doUpdate(self, c):
+        model = self.model
+        
+        if len(c.cpt) == 0:
+            print "no contact yet"
+        else:
+            # That what we get 
+            print "Nb of contact: ", len(c.cpt)
+            print "for contact 0: "
+            cc = c.cpt[0]
+            print "    ai :", cc.ai[:]
+            print "    aj :", cc.aj[:]
+            print "    gap:", cc.gap
+            print "    ni :", cc.ni[:]
+            print "    nj :", cc.nj[:]
+            print "    normalForce:", cc.normalForce
+            time.sleep(.1)
+
+        tau = lgsm.zeros(model.nbInternalDofs())
+        self.tau_out.write(tau)
--- source/examples/04_contact/common/common.py
+++ source/examples/04_contact/common/common.py
@@ -5,19 +5,21 @@ import loader
 import deploy
 deploy.loadTypekitsAndPlugins()
 
+##### import for clock agent
+import deploy.deployer as ddeployer
+import dsimi.rtt
 
 ##### Create physic and graphic agents
 import agents.physic.core
 import agents.graphic.simple
 import agents.graphic.proto
 
-time_step=.01
 
 # Physic
-def get_physic_agent():
-    phy = agents.physic.core.createAgent("physic", 0)                        # physic agent
-    ms = agents.physic.core.createGVMScene(phy, "main", time_step=time_step) # physic scene
-    lmd = agents.physic.core.createXCDScene(phy, "xcd", "LMD", lmd_max=.01)  # collision scene
+def get_physic_agent(time_step=.01, lmd_max=.01):
+    phy = agents.physic.core.createAgent("physic", 0)                               # physic agent
+    ms = agents.physic.core.createGVMScene(phy, "main", time_step=time_step)        # physic scene
+    lmd = agents.physic.core.createXCDScene(phy, "xcd", "LMD", lmd_max=lmd_max)     # collision scene
 
     ms.setGeometricalScene(lmd) # link physic scene and collision scene
     phy.s.setPeriod(time_step)
@@ -26,16 +28,69 @@ def get_physic_agent():
 
 
 # Graphic
-def get_graphic_agent():
+def get_graphic_agent(enableNavigation=True, showGround=True):
     graph = agents.graphic.simple.createAgent("graphic", 0)                          # graphic scne
     gInterface, Nscene, Nwin, Nview = agents.graphic.simple.setupSingleGLView(graph) # graphic interface
     agents.graphic.proto.configureBasicLights(gInterface)
     agents.graphic.proto.configureBasicCamera(gInterface)
-    graph.s.Viewer.enableNavigation(True)
-    gInterface.SceneryInterface.showGround(True)
+    graph.s.Viewer.enableNavigation(enableNavigation)
+    gInterface.SceneryInterface.showGround(showGround)
     
     return graph, gInterface
 
 
+def get_clock_agent(time_step=0.01):
+    ##### Create clock
+    clock = dsimi.rtt.Task(ddeployer.load("clock", "dio::Clock", "dio-cpn-clock", "dio/component/"))
+    clock.s.setPeriod(time_step)
+    return clock
+
+    
+    
+# Connection between agents
+def connnectBodyState(phy, graph, world):
+    ocb = phy.s.Connectors.OConnectorBodyStateList.new("ocb", "body_state")
+    graph.s.Connectors.IConnectorBody.new("icb", "body_state_H", "mainScene")
+    graph.getPort("body_state_H").connectTo(phy.getPort("body_state_H"))
+
+    # add markers on bodies
+    for b in world.scene.rigid_body_bindings:
+        if len(b.graph_node) and len(b.rigid_body):
+            ocb.addBody(str(b.rigid_body))
+
+    return ocb
+
+
+def connnectContactVisualization(phy, graph, contactPairs, maxProximity=.05, glyphScale=2.):
+
+    occ = phy.s.Connectors.OConnectorContactBody.new("occ", "contacts")
+    icc = graph.s.Connectors.IConnectorContacts.new("icc", "contacts", "mainScene")
+    graph.getPort("contacts").connectTo(phy.getPort("contacts"))
+    
+    icc.setMaxProximity(maxProximity)
+    icc.setGlyphScale(glyphScale)
+
+    
+    body_names = phy.s.GVM.Scene('main').getBodyNames()
+    for elem1, elem2 in contactPairs:
+        
+        if elem1 in body_names:
+            L1 = [elem1]
+        else:
+            r1 = phy.s.GVM.Robot(elem1)
+            L1 = [r1.getSegmentRigidBody2(n) for n in r1.getSegmentNames()]
+
+        if elem2 in body_names:
+            L2 = [elem2]
+        else:
+            r2 = phy.s.GVM.Robot(elem2)
+            L2 = [r2.getSegmentRigidBody2(n) for n in r2.getSegmentNames()]
+
+        for e1 in L1:
+            for e2 in L2:
+                occ.addInteraction(e1, e2)
+
+    
+    return occ, icc
 
 
