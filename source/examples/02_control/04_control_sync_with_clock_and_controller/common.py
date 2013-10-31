02dbf0102868897288a10d2beb17819857517de1
shak <sovannara.hak@isir.upmc.fr>
2013-10-31 15:57:13 +0100

    Fix control/control_sync_with_clock_and_controller

--- source/examples/02_control/04_control_sync_with_clock_and_controller/common.py
+++ source/examples/02_control/04_control_sync_with_clock_and_controller/common.py
@@ -39,8 +39,7 @@ def get_graphic_agent():
 
 
 import desc.scene
-import desc.robot
-
+import desc.physic
 
 # Robot
 def create_pendulum(world, p_name="pendulum", p_init=None):
@@ -53,30 +52,28 @@ def create_pendulum(world, p_name="pendulum", p_init=None):
         p_init = lgsm.Displacement()
 
     kin_tree = (body[0], mass, p_init, [], [
-                  (body[1], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
-                    (body[2], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
-                      (body[3], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], []
+        (body[1], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
+            (body[2], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
+                (body[3], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], []
                     )]
-                  )]
-                )])
+                )]
+            )])
 
 
-    # Create a description of the kinematic tree
-    desc.robot.addKinematicTree(world.scene.physical_scene,
-                                parent_node=None,
-                                tree=kin_tree,
-                                fixed_base=True,
-                                H_init=lgsm.Displacement())
+        # Create a description of the kinematic tree
+    desc.physic.fillKinematicTree(world.scene.physical_scene.nodes.add(),
+            tree=kin_tree,
+            fixed_base=True,
+            H_init=lgsm.Displacement())
 
     desc.physic.addMechanism(world.scene.physical_scene,
-                             name=p_name,
-                             root_node=body[0],
-                             trim_nodes=[],
-                             bodies=body,
-                             segments=body)
+            name=p_name,
+            root_node=body[0],
+            trim_nodes=[],
+            bodies=body,
+            segments=body)
 
 
-import desc.scene
 import agents.graphic.builder
 import agents.physic.builder
 
