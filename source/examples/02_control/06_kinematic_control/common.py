a8380304d5416ab18c8b6fdf509a38b009110df5
shak <sovannara.hak@isir.upmc.fr>
2013-10-31 17:09:49 +0100

    Fix control/kinematic_control

--- source/examples/02_control/06_kinematic_control/common.py
+++ source/examples/02_control/06_kinematic_control/common.py
@@ -39,8 +39,7 @@ def get_graphic_agent():
 
 
 import desc.scene
-import desc.robot
-
+import desc.physic
 
 # Robot
 def create_pendulum(world, p_name="pendulum", p_init=None):
@@ -53,33 +52,30 @@ def create_pendulum(world, p_name="pendulum", p_init=None):
         p_init = lgsm.Displacement()
 
     kin_tree = (body[0], mass, p_init, [], [
-                  (body[1], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
-                    (body[2], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
-                      (body[3], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], []
+        (body[1], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
+            (body[2], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], [
+                (body[3], mass, lgsm.Displacementd(.5,0,0,1,0,0,0), [('hinge',[0,0,0],[0,1,0], damp, -3.14, 3.14, 0.2)], []
                     )]
-                  )]
-                )])
+                )]
+            )])
 
 
-    # Create a description of the kinematic tree
-    desc.robot.addKinematicTree(world.scene.physical_scene,
-                                parent_node=None,
-                                tree=kin_tree,
-                                fixed_base=True,
-                                H_init=lgsm.Displacement())
+        # Create a description of the kinematic tree
+    desc.physic.fillKinematicTree(world.scene.physical_scene.nodes.add(),
+            tree=kin_tree,
+            fixed_base=True,
+            H_init=lgsm.Displacement())
 
     desc.physic.addMechanism(world.scene.physical_scene,
-                             name=p_name,
-                             root_node=body[0],
-                             trim_nodes=[],
-                             bodies=body,
-                             segments=body)
+            name=p_name,
+            root_node=body[0],
+            trim_nodes=[],
+            bodies=body,
+            segments=body)
 
 
-import desc.scene
 import agents.graphic.builder
 import agents.physic.builder
-
 
 def create_world_and_deserialized(phy, ms, lmd, graph, gInterface):
 
@@ -87,13 +83,15 @@ def create_world_and_deserialized(phy, ms, lmd, graph, gInterface):
 
     # add some pendulums
     create_pendulum(world, "p1", lgsm.Displacement())
+    create_pendulum(world, "p2", lgsm.Displacement(0,.5,0,1,0,0,0))
+    create_pendulum(world, "p3", lgsm.Displacement(0,1.,0,1,0,0,0))
 
     ##### Deserialize world: register world description in phy & graph agents
     agents.graphic.builder.deserializeWorld(graph, gInterface, world)
     agents.physic.builder.deserializeWorld(phy, ms, lmd, world)
     
     ##### Configure some robots
-    phy.s.GVM.Robot("p1").enableGravity(False)
+    phy.s.GVM.Robot("p2").enableGravity(False)
     
     return world
 
